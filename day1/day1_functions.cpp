35ca7f98eeb6057f0e7eb1b9c67e05ab2a8cf70b
lyu <lyu@users.noreply.github.com>
2019-11-04 13:42:27 -0500

    update function overloading example

--- day1/day1_functions.cpp
+++ day1/day1_functions.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include <cmath>        // The C math library, we import this for the std::pow power function
 
+
+// Let's create a function that takes an integer, multiply it by 2, and returns the result
+// Almost everything in C++ is explicity typed, so we use the 'int' keyword twice to indicate
+// the input and return types
+int foo(int n) {
+    return 2 * n;
+}
+
+// The foo function above only takes integer arguments, if you call it with foo(6.1), C++
+// will convert the floating point number 6.1 to integer 6 by discarding everything after
+// the decimal point and you will get 12 as the result
+// We can define another foo function with a different pair of input/return types
+// C++ is smart enough to choose the right function for the right input type
+// Try uncomment the function below and observe the different output!
+// float foo(float f) {
+//     return 2.0 * f;
+// }
+
 // Here we create a square function, that takes one integer as the input (hence the (int x))
 // and returns an integer as the output
+// Don't use Python's "**" operator in C++!
 int square(int x) {
     return std::pow(x, 2);
 }
@@ -20,4 +39,9 @@ int main() {
     std::cout << "cubic(2) = " << cubic(2) << '\n';
     std::cout << "cubic(square(2)) = (2^2)^3 = " << cubic(square(2)) << '\n';
     std::cout << "square(cubic(2)) = (2^3)^2 = " << square(cubic(2)) << '\n';
+
+    int a = 5;
+    float b = 6.1;
+    std::cout << "foo(5) = " << foo(a) << '\n';
+    std::cout << "foo(6.1) = " << foo(b) << '\n';
 }
--- day1/day1_functions.py
+++ day1/day1_functions.py
@@ -1,8 +1,11 @@
+def foo(n):
+    return 2 * n
+
 def square(x):
-    return x ** 2;
+    return x ** 2
 
 def cubic(x):
-    return x ** 3;
+    return x ** 3
 
-print(square(cubic(2)));
-print(cubic(square(2)));
+print(square(cubic(2)))
+print(cubic(square(2)))
